b8f70f4817d196a3f4bd8069d58c9fa8d066f922
Ilia Kaftanov <ilya.kaftanov@indriver.com>
2024-06-28 19:55:32 +0300

    fix: break to long while loop

--- jupnotipy/core.py
+++ jupnotipy/core.py
@@ -113,6 +113,7 @@ def login(bot_token: str, polling_freq: int = 3, num_retries: int = 10):
                     # from telegram side.
                     if awaited_text in message_text:
                         chat_id = message["chat"]["id"]
+                        break
         time.sleep(polling_freq)
         num_retries -= 1
         logger.info(
